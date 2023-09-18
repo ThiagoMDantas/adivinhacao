6a3543579e6ff1edc38fc8616967c814f031a9bc
Thiago Martins Dantas Silva <thiagomarins.dantas@gmail.com>
2023-09-18 04:21:27 -0300

    Update js

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
-const randomNumber = Math.round(Math.random()*10)
-let tentativa = 0;
+let randomNumber = Math.round(Math.random()*10);
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btn1 = document.querySelector("#btnTry");
+const btn2 = document.querySelector("#btnReset");
+let tentativa = 1;
 
 function handleClick(event){
   event.preventDefault()
-  tentativa++;
-  const valor = document.querySelector("#valor").value;
+
+  const valor = document.querySelector("#valor");
 
   console.log(randomNumber)
-
-  if(Number(valor) == randomNumber){
-    document.querySelector(".screen1").classList.add("hide");
-    document.querySelector(".screen2").classList.remove("hide");
-
-    document.querySelector(".screen2").innerText = `Acertou em ${tentativa} tentativas.`
+  if(Number(valor.value) == randomNumber){
+    toggleScreen();
+    document.querySelector("h2").innerText = `Acertou em ${tentativa} tentativas.`
+    
   }
-
+  valor.value = "";
+  tentativa++;
 
 }
+
+function handleResetClick(){
+  toggleScreen();
+  tentativa = 1;
+  randomNumber = Math.round(Math.random()*10);
+}
+
+function toggleScreen(){
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
+
+}
+
+btn1.addEventListener('click',handleClick)
+btn2.addEventListener('click',handleResetClick)
